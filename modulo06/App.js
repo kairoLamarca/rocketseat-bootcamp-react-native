eaf6a326be454b213da0ff360d37dae571842b8b
kairoLamarca <lamarca.kairo@gmail.com>
2020-01-31 13:19:51 -0300

    COnfiguração do eslint, prettier e editorconfig

--- modulo06/App.js
+++ modulo06/App.js
@@ -1,37 +1,14 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 
-import React, {Component} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-export default class App extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Teste app</Text>
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <View>
+      <Text>Teste app</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
