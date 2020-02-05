25aff530bd623db0b05a10c020913c4e2a09a7ca
kairoLamarca <lamarca.kairo@gmail.com>
2020-02-04 21:46:27 -0300

    Configuração do React navigation

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,23 +1,9 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 
 import './config/ReactotronConfig';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
-
-console.tron.log('Hello World');
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Teste app</Text>
-    </View>
-  );
+  return <Routes />;
 }
